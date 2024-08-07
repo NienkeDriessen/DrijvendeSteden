c4c13fa0c427ec02bc5b9f0a8193c9f063d55e97
Martijn Smits <martijn.smits@outlook.com>
2024-08-07 18:14:41 +0200

    remove dist from git

--- city-viewer/main.js
+++ city-viewer/main.js
@@ -5,54 +5,58 @@ import { createSky } from './js/sky';
 import { createSun } from './js/sun';
 import { createCity } from './js/city';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+async function init() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-camera.position.set(0, 50, 80);
-const controls = createControls()
+    camera.position.set(0, 50, 80);
+    const controls = createControls()
 
-// Create environment
-const water = createOcean(scene)
-const sky = createSky(scene)
-createSun(scene, renderer, sky, water)
+    // Create environment
+    const water = createOcean(scene)
+    const sky = createSky(scene)
+    createSun(scene, renderer, sky, water)
 
-// Create city
-const city = await createCity(scene)
+    // Create city
+    const city = await createCity(scene)
 
-function createControls() {
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.25;
-    controls.screenSpacePanning = false;
-    controls.minDistance = 20;
-    controls.maxDistance = 80;
-    controls.maxPolarAngle = (Math.PI / 2) - 0.2;
-    
-    return controls
+    function createControls() {
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.enableDamping = true;
+        controls.dampingFactor = 0.25;
+        controls.screenSpacePanning = false;
+        controls.minDistance = 20;
+        controls.maxDistance = 80;
+        controls.maxPolarAngle = (Math.PI / 2) - 0.2;
+        
+        return controls
+    }
+
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+
+    function animateCity() {
+        const time = performance.now() * 0.001;
+        city.position.y = Math.sin( time ) * 0.3;
+    }
+
+    function animate() {
+        water.material.uniforms[ 'time' ].value += 1.0 / 180.0;
+        animateCity()
+        controls.update();
+
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+
+    animate();
 }
 
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-function animateCity() {
-    const time = performance.now() * 0.001;
-    city.position.y = Math.sin( time ) * 0.3;
-}
-
-function animate() {
-    water.material.uniforms[ 'time' ].value += 1.0 / 180.0;
-    animateCity()
-    controls.update();
-
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
-animate();
+init()

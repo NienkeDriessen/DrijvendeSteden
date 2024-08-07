a3c7835bce16d133cfab4e4c1451721c1a83b5a3
Martijn Smits <martijn.smits@outlook.com>
2024-08-07 18:16:46 +0200

    added link generation

--- city-recognition/gateway.py
+++ city-recognition/gateway.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template, redirect
 from recognizer import recognize_city
 from firebase import firebase
 import os
+import qrcode
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -14,7 +16,7 @@ def upload_image():
         if request.files:
             image = request.files["image"]
             id = create_result(image)
-            return id
+            return create_link(id)
     return render_template("upload_image.html")
 
 
@@ -28,3 +30,20 @@ def create_result(image):
 
     result = firebase.post('/data', grid)
     return result['name']
+
+def create_link(id):
+    link = f"https://66b39d39708f9b1e02b6d9eb--singular-granita-604f65.netlify.app/?id={id}"
+
+    qr = qrcode.QRCode(version=3, box_size=20, border=10, error_correction=qrcode.constants.ERROR_CORRECT_H)
+    qr.add_data(link)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    qr_path = "static/qr_code.png"
+    if not os.path.exists("static"):
+        os.makedirs("static")
+
+    img.save(qr_path)
+
+    return render_template('show_link.html', link=link, qr_path=qr_path)
+    

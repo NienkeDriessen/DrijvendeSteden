ebd9fe5707d9bcfe30139d1694f8b555e1c2be8f
Martijn Smits <martijn.smits@outlook.com>
2024-08-07 17:07:33 +0200

    upload to firebase

--- city-recognition/gateway.py
+++ city-recognition/gateway.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, render_template, redirect
 from recognizer import recognize_city
+from firebase import firebase
+import uuid
 import os
 
 app = Flask(__name__)
 
 app.config["IMAGE_UPLOAD"] = "upload/img.png"
+firebase = firebase.FirebaseApplication('https://drijvendesteden-default-rtdb.europe-west1.firebasedatabase.app/', None)
 
 @app.route("/", methods=["GET", "POST"])
 def upload_image():
@@ -13,7 +16,8 @@ def upload_image():
             image = request.files["image"]
             store_image(image)
             grid = recognize_city(app.config["IMAGE_UPLOAD"])
-            return grid
+            id = store_result(grid)
+            return id
     return render_template("upload_image.html")
 
 def store_image(image):
@@ -21,3 +25,18 @@ def store_image(image):
         os.makedirs("upload")
 
     image.save(app.config["IMAGE_UPLOAD"])
+
+def store_result(grid):
+    id = generate_id()
+    new_city = {str(id) : grid}
+    result = firebase.post('/data', new_city)
+    print(result)
+    return id
+
+def generate_id():
+    id = str(uuid.uuid4())
+    result = firebase.get(f'/data/{id}', None)
+    if result == None:
+        return id
+    else:
+        return generate_id()

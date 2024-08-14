2f400df109889b5d9f470434f414979569fd7a6d
Martijn Smits <martijn.smits@outlook.com>
2024-08-14 16:26:08 +0200

    implemented own k-means for space constraints on server and updated requirements

--- city-recognition/recognizer/color_recognizer.py
+++ city-recognition/recognizer/color_recognizer.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-from sklearn.cluster import KMeans
 from .util import Color
 
 def closest_color_name(color):
@@ -15,16 +14,34 @@ def closest_color_name(color):
             
     return closest_name
 
+def initialize_centroids(pixels, k):
+    return pixels[np.random.choice(pixels.shape[0], k, replace=False)]
+
+def assign_clusters(pixels, centroids):
+    distances = np.linalg.norm(pixels - centroids[:, np.newaxis], axis=2)
+    return np.argmin(distances, axis=0)
+
+def update_centroids(pixels, clusters, k):
+    new_centroids = np.array([pixels[clusters == i].mean(axis=0) for i in range(k)])
+    return new_centroids
+
+def kmeans(pixels, k, max_iters=100, tol=1e-4):
+    centroids = initialize_centroids(pixels, k)
+    for _ in range(max_iters):
+        clusters = assign_clusters(pixels, centroids)
+        new_centroids = update_centroids(pixels, clusters, k)
+        if np.all(np.abs(new_centroids - centroids) < tol):
+            break
+        centroids = new_centroids
+    return centroids
+
 def get_dominant_color(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     pixels = img.reshape(-1, 3)
     non_black_pixels = pixels[np.any(pixels != [0, 0, 0], axis=1)]
+    dominant_color = kmeans(non_black_pixels, k=1)[0]
+    return dominant_color.astype(int)
 
-    kmeans = KMeans(n_clusters=1)
-    kmeans.fit(non_black_pixels)
-    colors = kmeans.cluster_centers_
-    
-    return colors.astype(int)[0]
 
 def get_color(img):
     color = get_dominant_color(img)

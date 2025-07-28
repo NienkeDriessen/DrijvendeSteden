b81a31eeecf95ae67e23d948edf4fd66ac300c96
Sahir Sujahudeen <m.s.sujahudeen@student.tudelft.nl>
2025-07-28 16:16:38 +0200

    added https

--- city-viewer/vite.config.js
+++ city-viewer/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
+    // Enable HTTPS. Vite will automatically generate a self-signed certificate.
+    https: true,
     // This allows Vite to accept requests from your public server address.
+    // Note: It's better to be specific with hostnames than allowing all.
     allowedHosts: [
       'sciencecentreontour.tudelft.nl'
     ],

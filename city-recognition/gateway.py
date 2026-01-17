4b2e200dbbe31cb83769e7d1a6aa7fc85602c787
Sahir Sujahudeen <m.s.sujahudeen@student.tudelft.nl>
2026-01-17 14:05:16 +0530

    Added some more input verification info

--- city-recognition/gateway.py
+++ city-recognition/gateway.py
@@ -123,7 +123,7 @@ viewer_engine = create_engine(f'sqlite:///{VIEWER_DB_PATH}')
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 app.config['PREFERRED_URL_SCHEME'] = os.getenv('PREFERRED_URL_SCHEME', 'https')
 FRONTEND_BASE_URL = os.getenv('VIEWER_BASE_URL', 'http://localhost:4173')
-FRONTEND_ORIGIN = os.getenv('FRONTEND_ORIGIN', '*')
+FRONTEND_ORIGIN = os.getenv('FRONTEND_ORIGIN', 'https://sciencecentreontour.tudelft.nl')
 
 # Create viewer table if not exists
 with viewer_engine.connect() as conn:
